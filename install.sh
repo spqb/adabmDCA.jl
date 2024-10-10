9615679bfd773e8a684824b4e49814e51906566c
Roberto Netti <robertonetti3@gmail.com>
2024-10-10 17:57:32 +0200

    some changes

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+echo "Downloading executable scripts..."
 wget -O adabmDCA.sh https://raw.githubusercontent.com/spqb/adabmDCA.jl/refs/heads/main/adabmDCA.sh
 wget -O execute.jl https://raw.githubusercontent.com/spqb/adabmDCA.jl/refs/heads/main/execute.jl
-
 chmod +x adabmDCA.sh
 
 # Install ArgParse and adabmDCA.jl from the GitHub repo
+echo "Installing julia dependencies..."
 julia --eval 'using Pkg; Pkg.add("ArgParse"); Pkg.add(PackageSpec(url="https://github.com/spqb/adabmDCA.jl"))'
